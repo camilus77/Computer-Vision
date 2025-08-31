1309bb92cda0a4b8c739b1d7846432b3d06803a1
Camilus <79711497+camilus77@users.noreply.github.com>
2025-08-31 09:03:36 +0100

    Update app.py

--- Attendance system using barcode/test/app.py
+++ Attendance system using barcode/test/app.py
@@ -1,11 +1,11 @@
-# app.py
 import time
+import streamlit as st
 import cv2
 import numpy as np
 import datetime as dt
 from pathlib import Path
 from pyzbar.pyzbar import decode
-import streamlit as st
+
 
 # -----------------------
 # App config & constants
